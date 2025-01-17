5d80b06f3f0c13d128b8d75f3303d7fc7d84d63a
dt256 <buiducthang13022004@gmail.com>
2025-01-17 08:37:03 +0700

    hidden status bar ver 2

--- app/src/main/java/com/ducthang/bttuan2/MainActivity.java
+++ app/src/main/java/com/ducthang/bttuan2/MainActivity.java
@@ -3,6 +3,8 @@ package com.ducthang.bttuan2;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         // Khởi tạo view
